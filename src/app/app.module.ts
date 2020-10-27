6dbf52cca9e16c48a21d8121a896bc5758be34c1
Pryimak-Volodya <kikimora11%08>
2020-10-27 14:37:12 +0200

    start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AnketaComponent } from './anketa/anketa.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AnketaComponent
   ],
   imports: [
     BrowserModule
