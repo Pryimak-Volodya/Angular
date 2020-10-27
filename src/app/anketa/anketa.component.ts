b88d88a04829c20a094b96c060fd0b295195754b
Pryimak-Volodya <kikimora11%08>
2020-10-27 15:04:37 +0200

    start

---
+++ src/app/anketa/anketa.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-anketa',
+  templateUrl: './anketa.component.html',
+  styleUrls: ['./anketa.component.css']
+})
+export class AnketaComponent implements OnInit {
+
+  public nameUnit = " ";
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
