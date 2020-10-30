b958092c77ca0f53aae55db7c0838ed8f277090a
Pryimak-Volodya <kikimora11%08>
2020-10-30 11:58:43 +0200

    finish3

--- src/app/anketa/anketa.component.ts
+++ src/app/anketa/anketa.component.ts
@@ -6,7 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./anketa.component.css']
 })
 export class AnketaComponent implements OnInit {
-
+ /*  Class Names  */
+  public content = 'allContent';
+  public head = 'headInfo';
+  public foto = 'blockFoto';
+  public message = 'message left';
+  public boxCentr = 'centr';
+  public boxLeft = 'w_50 left';
+  public boxRight = 'w_50 right';
+  public boxNumb = 'numBox left';
+  public boxSpan = 'text';
+  public boxSpan20 = 'text w_20 left';
+  public boxSpan30 = 'text w_30 left';
+  public boxInput = 'dataInput';
+  public boxInput20 = 'dataInput w_20 left';
+  public boxInputAuto = 'dataInput w_auto left';
   constructor() { }
 
   ngOnInit(): void {
