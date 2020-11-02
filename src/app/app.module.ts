c3aaf058ed34a20f7eb7537e40b30d83c97688da
Pryimak-Volodya <kikimora11%08>
2020-11-02 15:01:24 +0200

    finish5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { AnketaComponent } from './anketa/anketa.component';
+import { Anketa2Component } from './anketa2/anketa2.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    AnketaComponent
+    AnketaComponent,
+    Anketa2Component
   ],
   imports: [
     BrowserModule,
