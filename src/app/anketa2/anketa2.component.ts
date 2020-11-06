6addf618964df2695ee0aac623116291a9cb1ed4
Pryimak-Volodya <kikimora11%08>
2020-11-06 09:31:09 +0200

    Anketa2.1

--- src/app/anketa2/anketa2.component.ts
+++ src/app/anketa2/anketa2.component.ts
@@ -1,3 +1,4 @@
+import { style } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,67 +7,70 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./anketa2.component.css']
 })
 export class Anketa2Component implements OnInit {
-  public content = 'allContent';
+  public content ='allContent';
   public boxHead = 'headbox';
   public infoContent = 'infoContent';
-  public buroCheck = {id: 'buro', isOff: false };
-  public frontCheck = {id: 'front', isOff: false };
-  public predCheck = {id: 'pred', isOff: false };
-  public orgaCheck = {id: 'orga', isOff: false };
-  public otherCheck = {id: 'other', isOff: false };
+  public buroCheck = {id: 'buro', isOff: false, value:''};
+  public frontCheck = {id: 'front', isOff: false, value:''};
+  public predCheck = {id: 'pred', isOff: false, value:''};
+  public orgaCheck = {id: 'orga', isOff: false, value:''};
+  public otherCheck = {id: 'other', isOff: false, value:''};
   public dayData = {id: 'dayData'};
-  public punkt1 = 'punkt1';
-  public punkt2 = 'punkt2';
-  public punkt3 = 'punkt3';
-  public punkt4 = 'punkt4';
-  public punkt5 = 'punkt5';
-  public punkt6 = 'punkt6';
+  public punkt1 = {class:'punkt1'};
+  public punkt2 = {class:'punkt2', statusBlock: 'none'};
+  public punkt3 = {class:'punkt3', statusBlock: 'none'};
+  public punkt4 = {class:'punkt4', statusBlock: 'none'};
+  public punkt5 = {class:'punkt5', statusBlock: 'block'};
+  public punkt6 = {class:'punkt6', statusBlock: 'block'};
   public first = [
-    {id: 'first1'},
-    {id: 'first2'},
-    {id: 'first3'}
+    {id:'first1'},
+    {id:'first2'},
+    {id:'first3'}
   ];
   public second = [
-    {id: 'second1'},
-    {id: 'second2'}
+    {id:'second1'},
+    {id:'second2'}
   ];
   public third = [
-    {id: 'third1'},
-    {id: 'third2'},
-    {id: 'third3'},
-    {id: 'third4'},
-    {id: 'third5'}
+    {id:'third1'},
+    {id:'third2'},
+    {id:'third3'},
+    {id:'third4'},
+    {id:'third5'}
   ];
   public fourth = [
-    {id: 'fourth1'},
-    {id: 'fourth2'},
-    {id: 'fourth3'},
-    {id: 'fourth4'}
+    {id:'fourth1'},
+    {id:'fourth2'},
+    {id:'fourth3'},
+    {id:'fourth4'}
   ];
   public fifth = [
-    {id: 'fifth1'},
-    {id: 'fifth2'},
-    {id: 'fifth3'},
-    {id: 'fifth4'},
-    {id: 'fifth5'}
+    {id:'fifth1'},
+    {id:'fifth2'},
+    {id:'fifth3'},
+    {id:'fifth4'}, 
+    {id:'fifth5'}
   ];
   public sixth = [
-    {id: 'sixth1'},
-    {id: 'sixth2'},
-    {id: 'sixth3'},
-    {id: 'sixth4'},
-    {id: 'sixth5'}
+    {id:'sixth1'},
+    {id:'sixth2'},
+    {id:'sixth3'},
+    {id:'sixth4'}, 
+    {id:'sixth5'}
   ];
-  public coment = {id: 'coment' };
+  public coment = {id: 'coment', value:''};
   public srartColor = 'srartColor';
-  public statusBlock = 'none';
-  // tslint:disable-next-line:typedef
-  onCheck(event){
+  onCheckColor(event){
     this.srartColor = 'checkColor';
   }
-  // tslint:disable-next-line:typedef
-  onCheckPunkt(event){
-    this.statusBlock = 'block';
+  onCheckPunkt1(event){
+    this.punkt4.statusBlock = 'block';
+  }
+  onCheckPunkt2(event){
+    this.punkt2.statusBlock = 'block';
+  }
+  onCheckPunkt3(event){
+    this.punkt3.statusBlock = 'block';
   }
   constructor() { }
   ngOnInit(): void {
